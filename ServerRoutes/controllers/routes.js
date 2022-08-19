97aaf9a5d2c6b23968ffb7d1188a24e6962b6a01
sanjo lawal <sanjolawal600@gmail.com>
2022-08-19 01:55:56 -0700

    testing

---
+++ ServerRoutes/controllers/routes.js
@@ -0,0 +1,37 @@
+const path = require(`path`);
+
+const collection = require(path.resolve(`server.js`));
+
+console.log(collection);
+
+const tasksSent = (req, res) => {
+  const { task } = req.body;
+  console.log(task);
+  // collection.create({ task: task });
+  res.end();
+};
+
+const tasksList = async (req, res) => {
+  try {
+    const datas = await collection.find({});
+    res.send(datas);
+  } catch {
+    console.log(`error fetching all documents from database`);
+  }
+};
+
+const singleTask = (req, res) => {};
+const updateSingleTask = (req, res) => {
+  res.send(" put ok");
+};
+const deleteSingleTask = (req, res) => {
+  res.send("delete ok");
+};
+
+module.exports = {
+  tasksList,
+  tasksSent,
+  singleTask,
+  updateSingleTask,
+  deleteSingleTask,
+};
